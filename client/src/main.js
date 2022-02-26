67668fe52f6ae6e8f838d6a194954f453bed7eaf
hellgheast <hellgheast.public@gmail.com>
2022-02-26 18:10:20 +0100

    Upgraded to vuesj3

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,8 @@
-import Vue from 'vue'
+import { createApp } from 'vue';
 import App from './App.vue'
 import router from './router'
-// import { createPinia, PiniaVuePlugin } from 'pinia'
-
-Vue.config.productionTip = false
 
 // Vue.use(PiniaVuePlugin)
 // const pinia = createPinia()
 
-new Vue({
-  router,
-  render: h => h(App),
-//  pinia,
-}).$mount('#app')
+createApp(App).use(router).mount('#app')
